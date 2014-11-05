374dc227ec848351f5735415bc482020ed16645c
yawenchina <yawenchina@126.com>
2014-11-04 21:16:20 -0800

    rectangle

---
+++ rectangle.java
@@ -0,0 +1,14 @@
+public int largestRectangleArea(int[] height) {  
+    Stack<Integer> s = new Stack<Integer>();
+    for(int i = 0; i < height.length; i++){
+
+        while(!s.isEmpty() && height[i] >height[s.peek()]){
+          
+
+
+
+          
+        }
+
+    }
+ }  
